8e182fb52a28e311ef9d7ebee51dce6d3a491507
stephanie lucano <stephaniemarielucano@gmail.com>
2021-10-28 16:26:21 -0400

    Imported all the files within src/Components onto App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
-import "./index.css";
+import "./index.css"
+import "./Components/ContactUserCard"
+import "./Components/Contacts"
+import "./Components/NavBar"
+import "./Components/Post"
+import "./Components/Posts"
+import "./Components/UserProfile"
 
 const App = () => {
   return <p>Hello, world!</p>;
