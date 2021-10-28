896f887476c0790c1d5de620fbdd66294118e0cf
CarlosAMoran <Carlosamoran18@gmail.com>
2021-10-28 17:08:39 -0400

    Rearranged import to reflect file structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./index.css";
+import Contacts from "./Components/Contacts";
+import ContactUserCard from "./Components/ContactUserCard"
+import NavBar from "./Components/NavBar";
 import Post from "./Components/Post"
 import Posts from "./Components/Posts";
 import UserProfile from "./Components/UserProfile";
-import NavBar from "./Components/NavBar";
-import Contacts from "./Components/Contacts";
-import ContactUserCard from "./Components/ContactUserCard"
 
 const App = () => {
   return (
