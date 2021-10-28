56fb09faf736c4d4141cc1340ded61b77d000104
JoseBorbon <joserborbon@gmail.com>
2021-10-28 18:34:09 -0400

    add ContactUserCard Component

---
+++ src/Components/Contacts.js
@@ -0,0 +1,16 @@
+import './Contacts.css';
+import ContactUserCard from './ContactUserCard';
+
+const Contacts = () => {
+  return (
+    <div className="Contacts">
+      <h3>Contacts</h3>
+      <ContactUserCard />
+      <ContactUserCard />
+      <ContactUserCard />
+      <ContactUserCard />
+    </div>
+  );
+};
+
+export default Contacts;
