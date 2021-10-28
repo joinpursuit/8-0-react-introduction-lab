48593b9848e8138f66f86cb89d8134e4e709fab9
salrey <tekdehn@gmail.com>
2021-10-28 15:40:51 -0400

    Add NavBar Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import NavBar from "./Components/NavBar";
 import "./index.css";
 
 const App = () => {
-  return <p>Hello, world!</p>;
+  return (
+    <>
+      <NavBar/>
+    </>
+  );
 };
 
 export default App;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,13 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+    return (
+        <div className='Nav-Bar'>
+            <button>What is Pursuit?</button>
+            <button>Create an account</button>
+            <button>Sign in</button>
+        </div>
+    );
+};
+
+export default NavBar;
