d61fb6d3771ea79780916fca1259e51095df4601
Triane Peart <TrianePeart@gmail.com>
2021-10-28 17:10:20 -0400

    Completed Test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./index.css";
+import Contacts from "./Components/Contacts"
+import NavBar from "./Components/NavBar"
+import Posts from "./Components/Posts"
+import UserProfile from "./Components/UserProfile"
 
 const App = () => {
-  return <p>Hello, world!</p>;
+  return  (
+    <>
+    <NavBar/>
+    <Contacts/>
+    <Posts/>
+    <UserProfile/>
+    </>
+  )
+  
 };
 
 export default App;
