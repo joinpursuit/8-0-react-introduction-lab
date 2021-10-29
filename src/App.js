c7d55cf4b3ac421886927c23b6ad375bcfd5ff11
Fatema Kabir <fatema.kabir@pursuit.org>
2021-10-29 00:15:18 -0400

    User-Profile Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./index.css";
 import NavBar from "./Components/NavBar.js";
+import UserProfile from "./Components/UserProfile.js";
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <UserProfile />
     </div>   
   );
 };
---
+++ src/Components/UserProfile.js
@@ -0,0 +1,14 @@
+function UserProfile() {
+    return (
+        <div className="user-profile">
+          <img src="profile.png" height="160px" align="left"/> 
+             <section class="userText">
+                <h3>{'Jordan Walke'}</h3>
+                <h4>{'React Creator'}</h4>
+                <p>{'Lorem Ipsem'}</p>
+             </section>
+        </div>
+    );
+}
+
+export default UserProfile;
