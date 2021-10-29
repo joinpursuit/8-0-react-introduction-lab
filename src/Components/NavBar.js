496449206ca36bc2f86f2eda51bf3cc6e934691a
stephanie lucano <stephaniemarielucano@gmail.com>
2021-10-28 20:52:29 -0400

    Added an anonymous functions with three forms

---
+++ src/Components/NavBar.js
@@ -0,0 +1,17 @@
+const NavBar = () => {
+    return (
+        <div>
+            <form>
+                <input type='submit' value='What is Pursuit' />
+            </form>
+            <form>
+                <input type='submit' value='Create an account' />
+            </form>
+            <form>
+                <input type='submit' value='Sign In' />
+            </form>
+        </div>
+    );
+}
+
+export default NavBar;
