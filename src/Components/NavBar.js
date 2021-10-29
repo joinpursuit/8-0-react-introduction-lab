097ced0356b4be57e86dda69e9e36cea9ce84f32
stephanie lucano <stephaniemarielucano@gmail.com>
2021-10-29 11:16:53 -0400

    Added a form with three buttons to the nav-bar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,11 @@
 const NavBar = () => {
     return (
         <div>
-            <div>NavBar</div>
+            <form>
+                <input type="submit" value="What is Pursuit?" />
+                <input type="submit" value="Create an account" />
+                <input type="submit" value="Sign in" />
+            </form>
         </div>
     );
 }
