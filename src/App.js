1237aac25cbe7d7464e8428a8cf304c9dc52a507
DylanLovell <arcanyne7@gmail.com>
2021-10-28 23:36:47 -0400

    Posts v1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './index.css';
 import NavBar from './Components/NavBar.js';
-import UserProfile from './Components/UserProfile';
+import UserProfile from './Components/UserProfile.js';
+import Posts from './Components/Posts.js';
+
 const App = () => {
   return (
     <>
       <NavBar />
       <UserProfile />
+      <Posts />
     </>
   );
 };
---
+++ src/Components/Posts.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Post from './Post';
+
+class Posts extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <h2>Posts</h2>
+      </div>
+    );
+  }
+}
+
+export default Posts;
