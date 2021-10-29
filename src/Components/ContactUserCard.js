c76df599f1295db0a3809139bcdd8f38cbc074c0
stephanie lucano <stephaniemarielucano@gmail.com>
2021-10-29 12:19:42 -0400

    Prepopulated the ContactUserCard.js with the same tags and information displayed as UserProfile.js

--- src/Components/ContactUserCard.js
+++ src/Components/ContactUserCard.js
@@ -1,10 +1,10 @@
-const ContactUserCard = () {
+const ContactUserCard = () => {
     return (
         <div>
-            <div>ContactUserCard</div>
-            <div>ContactUserCard</div>
-            <div>ContactUserCard</div>
-            <div>ContactUserCard</div>
+            <img src="profile.png" alt="the silhouette a person with a gray background"></img>
+            <h2 className="full-name">Jordan Walke</h2>
+            <h3 className="job-title">React Creator</h3>
+            <p className="bio">Lorem Ipsem</p>
         </div>
     );
 }
