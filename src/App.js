ffbcfd97f5f685ff481b0e71a53f68c7e22fd132
DylanLovell <arcanyne7@gmail.com>
2021-10-29 09:39:41 -0400

    Contacts v1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './index.css';
 import NavBar from './Components/NavBar.js';
 import UserProfile from './Components/UserProfile.js';
 import Posts from './Components/Posts.js';
+import Contacts from './Components/Contacts.js';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <NavBar />
       <UserProfile />
       <Posts />
+      <Contacts />
     </>
   );
 };
---
+++ src/Components/Contacts.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Contacts extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <h2>Contacts</h2>
+      </div>
+    );
+  }
+}
+
+export default Contacts;
