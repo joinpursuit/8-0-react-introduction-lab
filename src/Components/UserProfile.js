bc1e264098af47cbd5f796df82a779cd4b97ee84
stephanie lucano <stephaniemarielucano@gmail.com>
2021-10-29 11:57:21 -0400

    Added profile image tag along with h2, h3, and p tags for full name, job title and bio, respectively

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -1,7 +1,10 @@
 const UserProfile = () => {
     return (
         <div>
-            <div>UserProfile</div>
+            <img src="profile.png" alt="the silhouette a person with a gray background"></img>
+            <h2 className="full-name">Jordan Walke</h2>
+            <h3 className="job-title">React Creator</h3>
+            <p className="bio">Lorem Ipsem</p>
         </div>
     );
 }
