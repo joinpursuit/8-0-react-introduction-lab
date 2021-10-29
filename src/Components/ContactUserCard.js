c927d025d50fc1d7848ebef99c7e4f2220de58fa
HectorIlarraza <53978521+HectorIlarraza@users.noreply.github.com>
2021-10-29 17:01:07 -0400

    Lab Completed

---
+++ src/Components/ContactUserCard.js
@@ -0,0 +1,5 @@
+const ContactUserCard = () =>{
+    return <div>ContactUserCard</div>
+}
+
+export default ContactUserCard;
---
+++ src/Components/Posts.js
@@ -0,0 +1,15 @@
+import Post from "./Post";
+
+const Posts = () =>{
+    return ( 
+    <div>
+        <h3>Posts</h3>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+    </div>
+    )
+}
+
+export default Posts;
