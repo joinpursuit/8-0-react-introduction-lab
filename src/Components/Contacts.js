8f850e346bddf1d049cd40cf13de068dc56ddc32
Lakisha J <lakishajohnson@pursuit.org>
2021-10-28 22:26:28 -0400

    Import Contacts

---
+++ src/Components/Contacts.js
@@ -0,0 +1,13 @@
+import ContactUserCard from "./ContactUserCard";
+
+function Contacts() {
+    return (
+      <div className = "contacts">
+       <ContactUserCard/>
+       <ContactUserCard/>
+       <ContactUserCard/>
+      </div>
+    )
+  }
+
+ export default Contacts
