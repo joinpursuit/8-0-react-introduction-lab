c7abf7827cb8c38d02e9d8891e431d129df24740
stephanie lucano <stephaniemarielucano@gmail.com>
2021-10-29 13:00:52 -0400

    Separted the input type buttons into three forms

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,7 +3,11 @@ const NavBar = () => {
         <div className="NavBar">
             <form>
                 <input type="submit" value="What is Pursuit?" />
-                <input type="submit" value="Create an account" />
+            </form>
+            <form>
+                <input type="submit" value="Create an account" />   
+            </form>
+            <form>
                 <input type="submit" value="Sign in" />
             </form>
         </div>
