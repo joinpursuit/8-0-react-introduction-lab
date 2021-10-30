9219435877fdb0a24a346917e638ee9da8621029
Melissa Lopez <1zuzsuit492@Melissas-MacBook-Pro.local>
2021-10-29 22:29:41 -0400

    cypress not passing tests. issue with port 3000

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./index.css";
+import Contacts from "./Components/Contacts.js"
+import NavBar from "./Components/NavBar.js"
+import Posts from "./Components/Posts.js"
+import UserProfile from "./Components/UserProfile.js"
+//imported all files onto the app.js react file.//
 
 const App = () => {
-  return <p>Hello, world!</p>;
+  return (
+  <>
+<NavBar/>
+<Contacts/>
+<Posts/>
+<UserProfile/>
+</>
+  )
+  //returned all created app components./ /
 };
 
 export default App;
