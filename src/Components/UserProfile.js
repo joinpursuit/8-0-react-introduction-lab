1a726c862bfe6bcdb178cbe6b0f75de38d713dc2
Melissa Lopez <1zuzsuit492@Melissas-MacBook-Pro.local>
2021-10-29 22:24:05 -0400

    added stuff.

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -1,7 +1,10 @@
 function UserProfile(){
     return(
-        <div>
-            UserProfile
+        <div className='photo'>
+            <img src ='./profile.png' alt='default picture'> </img>
+            <h3>Contestant #1</h3>
+            <h4>React Creator</h4>
+            <p>lorem ipsum</p>
         </div>
     )
 }
