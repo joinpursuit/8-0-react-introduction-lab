2bc64541b0c85ad6fd8d74ee7f0c1dc0592d804f
Xiaoming Yang <yangxiaoming295@gmail.com>
2021-10-31 12:31:23 -0400

    create NavBar function in NavBar.js

---
+++ src/Components/NavBar.js
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return (
+        <NavBar className="navbar">
+            <button type="submit">button1</button>
+            <button type="submit">button2</button>
+            <button type="submit">button3</button>
+        </NavBar>
+    )
+}
+
+export default NavBar;
