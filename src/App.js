04c6e9d0130b36447d5e57e0e3d2e4db5f547c1f
Xiaoming Yang <yangxiaoming295@gmail.com>
2021-10-31 14:20:18 -0400

    create Post component in Post.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./index.css";
+import NavBar from "./Components/NavBar";
 
 const App = () => {
-  return <p>Hello, world!</p>;
+  return (
+    <p>Hello, world!</p>
+    // <div>      
+    //   <NavBar />
+    // </div> 
+  )
+  
 };
 
 export default App;
---
+++ src/Components/Post.js
@@ -0,0 +1,10 @@
+const Post = () => {
+    return (
+        <div>
+            <h2>This is a title</h2>
+            <p>Something else</p>
+        </div>
+    )
+}
+
+export default Post;
