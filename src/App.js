beb65780cddddaf0e1ced3e20dc86acfae179c74
Xiaoming Yang <yangxiaoming295@gmail.com>
2021-10-31 14:26:32 -0400

    create Posts component in Posts.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./index.css";
 import NavBar from "./Components/NavBar";
+import Posts from "./Components/Posts";
 
 const App = () => {
   return (
---
+++ src/Components/Posts.js
@@ -0,0 +1,14 @@
+import Post from "./Post";
+
+const Posts = () => {
+    return (
+        <div>
+            <h2>Posts</h2>
+            <Post />
+            <Post />
+            <Post />
+        </div>
+    )
+}
+
+export default Posts;
