8031e6466c35ff1597f976f466dd9cc7ad103c3f
Milton Clark <kingcoder247@gmail.com>
2021-11-03 17:02:08 -0400

    Create class component for NavBar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,18 @@
+const { Component } = require("react");
+
+class NavBar extends Component{
+    constructor(){
+        super()
+    };
+
+    render(){
+        return (
+            <div className="navbar">
+
+            </div>
+        )
+    }
+
+};
+
+export default NavBar;
