4bf19a6c145451098294d7d7b9f4049d9c257b6f
rae charles <r_charles87@yahoo.com>
2021-12-01 14:41:00 -0500

    fixed App js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./index.css";
+import Navbar from "./Components/NavBar.js"
+import UserProfile from "./Components/UserProfile";
+import Posts from "./Components/Posts";
+import Contacts from "./Components/Contacts";
 
 const App = () => {
-  return <p>Hello, world!</p>;
+  return (
+    <div>
+    <Navbar />
+    <UserProfile />
+    <Posts />
+    <Contacts />
+    </div>
+  )
 };
 
 export default App;
