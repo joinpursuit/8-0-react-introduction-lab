96ba4e21ccca326b6ff8399312cf65180a4881f9
Lortiz528 <lortiztech@gmail.com>
2022-05-05 11:55:32 -0400

    tests passing, may add css

---
+++ src/Components/Posts.js
@@ -0,0 +1,14 @@
+import Post from './Post';
+import './posts.css';
+
+const Posts = () => {
+  return (
+    <section className="posts">
+      <Post />
+      <Post />
+      <Post />
+    </section>
+  );
+};
+
+export default Posts;
