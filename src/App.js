842577c54cd684070beace6275ba0644b21a8765
Christine Taylor <ctay47@icloud.com>
2022-05-04 21:31:50 -0400

    react intro done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
+import NavBar from "./Components/NavBar.js"
+import UserProfile from "./Components/UserProfile.js";
+import Posts from "./Components/Posts.js"
+import Post from "./Components/Post.js"
+import ContactUserCard from "./Components/ContactUserCard.js";
+import Contacts from "./Components/Contacts.js";
 import "./index.css";
 
 const App = () => {
-  return <p>Hello, world!</p>;
+
+  return (
+    <div>
+      <NavBar />
+      <UserProfile />
+      <Posts />
+      <Post />
+      <ContactUserCard />
+      <Contacts />
+    </div>
+     
+   )
+      
 };
 
 export default App;
