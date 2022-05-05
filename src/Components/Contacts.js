8bb21ed103c975e003cabd4af6ad2aa5de97ec5d
Denisse Maldonado <denissemaldonado@Denisses-MacBook-Air.local>
2022-05-04 20:29:32 -0400

    tests pass only

---
+++ src/Components/Contacts.js
@@ -0,0 +1,13 @@
+import ContactUserCard from "./ContactUserCard";
+
+const Contacts = () => {
+    return (
+        <div className="ContactCards">
+            <ContactUserCard />
+            <ContactUserCard />
+            <ContactUserCard />
+        </div>
+    );
+};
+
+export default Contacts;
