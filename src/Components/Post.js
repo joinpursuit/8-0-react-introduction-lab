adecbfc87dcb8b276fa154ecb25363225d1561b9
KenyaMighty <kenyamighty@pursuit.org>
2022-05-04 20:43:58 -0400

    firstsuccess

---
+++ src/Components/Post.js
@@ -0,0 +1,6 @@
+const Post = () => {
+    return <p>Hi, I'm a Post!</p>;
+  };
+  
+  export default Post;
+  
---
+++ src/Components/UserProfile.js
@@ -0,0 +1,6 @@
+const UserProfile = () => {
+    return <p>Hi, I'm a UserProfile!</p>;
+  };
+  
+  export default UserProfile;
+  
