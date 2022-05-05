65312cf18c8de60d2e3d6d85c3425fa283d6a1cc
John Prado <jcprado20@icloud.com>
2022-05-04 21:33:11 -0400

    React ting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
+import NavBar from "./Components/NavBar.js"
+import UserProfile from "./Components/UserProfile.js";
+import Posts from "./Components/Posts.js"
+import Contacts from "./Components/Contacts.js";
 import "./index.css";
 
 const App = () => {
-  return <p>Hello, world!</p>;
+
+  return (
+    <div>
+      <NavBar />
+      <UserProfile />
+      <Posts />
+      <Contacts />
+    </div>
+     
+   )
+      
 };
 
 export default App;
+
---
+++ src/Components/UserProfile.js
@@ -0,0 +1,16 @@
+const user = {
+    name: "Jordan Walke",
+    occupation: "React Creator",
+    title: 'Lorem Ipsem'
+}
+const UserProfile = () => {
+    return (
+        <div className="profile">
+            {user.name}
+            {user.occupation}
+            {user.title}
+
+        </div>
+    )
+}
+export default UserProfile
