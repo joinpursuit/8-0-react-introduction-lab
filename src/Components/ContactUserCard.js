f08a519c15f358ec7c7255ae08f951a48f4de5ad
TaryneLeach <taryneleach@gmail.com>
2022-05-05 20:53:30 -0400

    Test passing, will keep working with css

---
+++ src/Components/ContactUserCard.js
@@ -0,0 +1,13 @@
+
+const ContactUserCard = () => {
+    return (
+       <div>
+           <img src="profile.png" alt="profile"></img>
+           <h3>Jordan Walke</h3>
+           <h4>React Creator</h4>
+           <p>Lorem Ipsem</p>
+       </div>
+    )
+}
+
+export default ContactUserCard
---
+++ src/Components/Post.js
@@ -0,0 +1,9 @@
+const Post = () => {
+    return (
+        <div>
+            <h4>Title</h4>
+            <p>Lorem Ipsem</p>
+        </div>
+    )
+}
+export default Post;
