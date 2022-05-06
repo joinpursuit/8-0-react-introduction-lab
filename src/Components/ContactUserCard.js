894d5b15e3f1264394cc14a6b2655655b712f0e5
bowersjuan <bowersjuan@gmail.com>
2022-05-06 01:44:58 -0400

    Update Contacts.js and ContactUserCard.js components

---
+++ src/Components/ContactUserCard.js
@@ -0,0 +1,11 @@
+const ContactUserCard = () => {
+  return (
+    <div>
+      <img src="../public/profile.png" alt="Profile Logo" />
+      <p>User Name</p>
+      <p>User Title</p>
+      <p>User Bio</p>
+    </div>
+  );
+};
+export default ContactUserCard;
