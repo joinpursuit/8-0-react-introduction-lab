8776c1ddb955606363c31e9393a77b8a45a857c9
Jonathan Scheiber <jon@scheiber.me>
2022-05-05 22:06:37 -0400

    add contact user cards to contacts

---
+++ src/Components/Contacts.js
@@ -0,0 +1,13 @@
+import ContactUserCard from "./ContactUserCard";
+const Contacts = () => {
+  return (
+    <div>
+      <h1>Contacts</h1>
+      <ContactUserCard></ContactUserCard>
+      <ContactUserCard></ContactUserCard>
+      <ContactUserCard></ContactUserCard>
+    </div>
+  );
+};
+
+export default Contacts;
