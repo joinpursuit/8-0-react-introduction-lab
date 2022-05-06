2acc30a94cce09be3ad2778d60e0f16b32a961a5
meera-ramesh19 <meeraramesh19@outlook.com>
2022-05-05 22:19:13 -0400

    lab completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from "react";
-import "./index.css";
+import React from 'react';
+import './index.css';
+import '../src/Components/App.css';
+import NavBar from './Components/NavBar.js';
+import UserProfile from './Components/UserProfile.js';
+import Contacts from './Components/Contacts.js';
+import Posts from './Components/Posts.js';
 
 const App = () => {
-  return <p>Hello, world!</p>;
+  return (
+    <div className='menu'>
+      <NavBar />
+      <div className='users-info'>
+      <UserProfile />
+      <Posts />
+      <Contacts />
+      </div>
+    </div>
+  );
 };
 
 export default App;
