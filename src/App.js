479f50b207a9a60856da435c14e9623eadbfec11
Matt M <Afrodesiacc@gmail.com>
2022-05-05 21:19:12 -0400

    Completed Lab

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
+import Posts from "./Components/Posts"
 import "./index.css";
+import NavBar from "./Components/NavBar"
+import UserProfile from "./Components/UserProfile";
+import Contacts from "./Components/Contacts";
 
 const App = () => {
-  return <p>Hello, world!</p>;
+  return <div>
+    <NavBar/>
+    <UserProfile/>
+    <main>
+      <Posts/>
+    </main>
+    <Contacts/>
+  </div>
 };
 
 export default App;
