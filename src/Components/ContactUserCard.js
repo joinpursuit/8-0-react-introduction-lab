01a3d20d6e847b1f4002041d13572e00cd38e738
bowersjuan <bowersjuan@gmail.com>
2022-05-06 02:32:26 -0400

    Update profile.png src link for ContactUserCard.js and UserProfile.js

--- src/Components/ContactUserCard.js
+++ src/Components/ContactUserCard.js
@@ -1,7 +1,7 @@
 const ContactUserCard = () => {
   return (
     <div className="border">
-      <img src="../public/profile.png" alt="Profile Logo" />
+      <img src="./profile.png" alt="Profile Logo" />
       <h4>User Name</h4>
       <p>User Title</p>
       <p>User Bio</p>
