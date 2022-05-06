0d422564cd03b9b74b69bc62eeb9a6e08ec28906
Andres Varon <anvaron@gmail.com>
2022-05-06 09:51:51 -0400

    Refactoring components

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -8,18 +8,18 @@ import React from "react";
     const arrayOfPosts = [
         {
           id: 0,
-          title: 'New Post',
-          content: 'Lorem Ipsum'
+          title: 'New Post Lorem ipsum',
+          content: 'Lorem ipsum dolor sit amet. Vel modi voluptas qui voluptatibus molestiae et deserunt enim sed ipsam suscipit. Et internos quia et sunt voluptatum sit quia dignissimos cum sequi repellat At inventore iure. In aliquam libero non maxime amet ea internos ipsum non quibusdam assumenda.'
         },
         {
           id: 1,
-          title: 'New Post',
-          content: 'Lorem Ipsum'
+          title: 'New Post Et pariatur',
+          content: 'Et pariatur corrupti hic pariatur harum qui aperiam natus et internos totam in vitae consectetur qui quas itaque cum totam consectetur? Eos voluptatem tempora aut nemo voluptatem et ducimus corrupti sit omnis Quis aut quos repellendus.'
         },
         {
           id: 2,
-          title: 'New Post',
-          content: 'Lorem Ipsum'
+          title: 'New Post Eos praesentium',
+          content: 'Eos praesentium rerum quo impedit quis et accusamus quis aut tempore vitae et iste deleniti ut ipsam sequi. Et sapiente reprehenderit non earum totam aut quisquam soluta qui neque modi in magni dolor At necessitatibus ducimus rem veritatis alias'
         },
         {
           id: 3,
@@ -53,8 +53,8 @@ export default function Post() {
     return (
         <>
         {arrayOfPosts.map(({ title, content, id }) => (
-            <div>
-                <h2 key={id}> {title} </h2><p> {content} </p>
+            <div className='entry'>
+                <h4 key={id}> {title} </h4><p> {content} </p>
             </div>
         ))}
         </>
