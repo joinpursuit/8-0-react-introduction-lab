381e26542cd55facabf9dd3216981c41b6b9a0cd
Isaac Gonzalez <u1337x@gmail.com>
2022-05-09 09:25:52 -0400

    Part 1

---
+++ src/Components/Contacts.js
@@ -0,0 +1,12 @@
+import ContactUserCard from "./ContactUserCard";
+const Contacts = () => {
+  return (
+    <div>
+      <p>Contacts</p>
+      <ContactUserCard />
+      <ContactUserCard />
+      <ContactUserCard />
+    </div>
+  );
+};
+export default Contacts;
