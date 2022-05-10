869a4197fd725d06cf8e56199f91f9de0f47fe73
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2022-05-10 12:27:48 -0400

    Final Commit

---
+++ src/Components/NavBar.js
@@ -0,0 +1,15 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return(
+     <header className="NavBar">
+
+        <button>What is Pursuit?{1+1}</button>
+        <button>Create an account</button>
+        <button>Sign in</button>
+
+    </header>
+    );  
+}
+
+export default NavBar
---
+++ src/Components/Post.js
@@ -0,0 +1,10 @@
+const Post = () => {
+    return (
+
+    <article>
+        <h3>Title</h3>
+        <p>Lorem Ipsem</p>
+    </article>
+    )
+}
+export default Post 
