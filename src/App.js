7879faddcd78b20d83f224fa0873f2221e679c06
King Ankoue <kingankoue@Kings-Air.home>
2022-05-24 10:57:39 -0400

    try1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
-import "./index.css";
+ import Posts from "./Components/Posts"
+ import "./index.css";
+ import NavBar from "./Components/NavBar"
+ import UserProfile from "./Components/UserProfile";
+ import Contacts from "./Components/Contacts";
 
-const App = () => {
-  return <p>Hello, world!</p>;
-};
+ const App = () => {
+   return <p>Hello, world!</p>;
+   return <div>
+     <NavBar/>
+     <UserProfile/>
+     <main>
+       <Posts/>
+     </main>
+     <Contacts/>
+   </div>
+ };
 
-export default App;
+ export default App
---
+++ src/Components/button.js
@@ -0,0 +1,6 @@
+const Button = (props) => {
+
+    return <button className="button">{props.buttonText}</button>
+}
+
+export default Button
