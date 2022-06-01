83b9c245f8f3048a64289882c636153fd0a67756
Lakisha Trusty <daprincessanime@gmail.com>
2022-06-01 14:46:53 -0400

    fixed my error message

---
+++ src/Components/NavBar.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class NavBar extends Component{
+    // constructor(){
+    //     super();
+    // }
+
+    render(){
+        return(
+            <div>
+               
+            </div>
+        )
+    }
+}
+export default NavBar;
