0b38e0f5544c747c2d1ce60137a4bcdfd9d72d24
TrianePeart <TrianePeart@gmail.com>
2022-10-04 14:11:32 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from "react";
-import "./index.css";
+import React from 'react';
+import './index.css';
+import Contacts from './Components/Contacts'
+import NavBar from './Components/NavBar'
+import Post from './Components/Posts'
 
 const App = () => {
-  return <p>Hello, world!</p>;
+  return (
+  <>
+  </>
+  
+  )
 };
 
 export default App;
