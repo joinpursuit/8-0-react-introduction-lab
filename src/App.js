a094b7483f193997ee4b527d4d4d4f1b8da44ccb
TrianePeart <TrianePeart@gmail.com>
2022-10-04 14:45:09 -0400

    Fixed app syntax error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.css';
 import Contacts from './Components/Contacts'
 import NavBar from './Components/NavBar'
-import Post from './Components/Posts'
+import Posts from './Components/Posts'
 import UserProfile from './Components/UserProfile'
 //Import the component files to app so it can read. 
 
@@ -10,7 +10,9 @@ const App = () => {
   return (
   <>
   <NavBar/>
- 
+  <UserProfile/>
+  <Posts/>
+  <Contacts/>
   </>
   )
 };
