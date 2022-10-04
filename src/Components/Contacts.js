8f1f1199629e4fbc04b3c0290ec5cbb3c552f467
Jonathan-Gaal <jonathangaal@pursuit.org>
2022-10-04 16:15:03 -0400

    passed

---
+++ src/Components/Contacts.js
@@ -0,0 +1,14 @@
+import ContactUserCard from "./ContactUserCard";
+const Contacts = () => {
+  return (
+    <div>
+      <h2>Contacts</h2>
+      <ContactUserCard />
+      <ContactUserCard />
+      <ContactUserCard />
+      <ContactUserCard />
+    </div>
+  );
+};
+
+export default Contacts;
