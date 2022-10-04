1d66b161cf43ba6b0b93f857f43db02c1d643728
TrianePeart <TrianePeart@gmail.com>
2022-10-04 14:16:33 -0400

    First Commit - With Notes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import './index.css';
 import Contacts from './Components/Contacts'
 import NavBar from './Components/NavBar'
 import Post from './Components/Posts'
+import UserProfile from './Components/UserProfile'
+//Import the component files to app so it can read. 
 
 const App = () => {
   return (
   <>
+  <NavBar/>
+  <UserProfile/>
+  <Post/>
+  <Contacts/>
   </>
+  //Return components according to the map. 
   
   )
 };
