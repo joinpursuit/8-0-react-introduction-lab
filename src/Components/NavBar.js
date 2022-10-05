50969ede8166483b4f7b23433c7cac01bf0eb9f0
hector918 <hector_zhong@outlook.com>
2022-10-04 21:30:11 -0400

    submit with css

---
+++ src/Components/NavBar.js
@@ -0,0 +1,11 @@
+const NavBar = () => {
+  return (
+    <div class="navbar" >
+      <button>What is Pursuit</button>
+      <button>Create an account</button>
+      <button>Sing in</button>
+    </div>
+  );
+};
+
+export {NavBar};
