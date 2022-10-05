1fb0d6adb1e0f45d306fbb563b8913106c8ba4fe
Gillesedgy <edgygilles@pursuit.org>
2022-10-05 11:43:29 -0400

    created-Contacts

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "./index.css";
 import NavBar from "./Components/NavBar";
 import UserProfile from "./Components/UserProfile";
 import Posts from "./Components/Posts";
-
+import Contacts from "./Components/Contacts";
 const App = () => {
   return (
     <div>
       <NavBar />
       <UserProfile />
       <Posts />
+      <Contacts></Contacts>
     </div>
   );
 };
--- src/Components/ContactUserCard.js
+++ src/Components/ContactUserCard.js
@@ -1,1 +1,11 @@
+const ContactUserCard = () => {
+  return (
+    <aside className="Contacts">
+      <h1>Name</h1>
+      <h6>Title</h6>
+      <p>Bio</p>
+    </aside>
+  );
+};
 
+export default ContactUserCard;
