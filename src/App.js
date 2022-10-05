8aa175ac47d66037976fcff720ad1db2c5a90b84
Karyn Huston <huston.karyn@gmail.com>
2022-10-04 22:07:20 -0400

    stopped

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./index.css";
+import NavBar from "./components/NavBar.js";
+import UserProfile from "./components/UserProfile.js";
+
 
 const App = () => {
-  return <p>Hello, world!</p>;
+  return (
+    <>
+  <p>Hello, world!</p>
+  <NavBar />
+  <UserProfile /> 
+  
+  </>
+  )
+
+
+
 };
 
 export default App;
---
+++ src/Components/Posts.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Post from "./components/Posts.js"
+
+const Posts = (Post) => {
+    return (
+    
+    )
+}
+
+export default Posts
