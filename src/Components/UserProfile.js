d5fc5e704f3c79b69f143dff6d631fefd0009483
Wwright91 <wisdomwright-martin@pursuit.org>
2022-10-05 17:44:22 -0400

    intro-complete

---
+++ src/Components/UserProfile.js
@@ -0,0 +1,12 @@
+const UserProfile = () => {
+  return (
+    <div className="profile">
+        <img
+          src="https://t4.ftcdn.net/jpg/03/46/93/61/360_F_346936114_RaxE6OQogebgAWTalE1myseY1Hbb5qPM.jpg"
+          alt="default-photo"
+        />
+    </div>
+  );
+};
+
+export default UserProfile;
