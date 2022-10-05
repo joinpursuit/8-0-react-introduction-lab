76017ca4129d1a425a45daf7eca71ef73247455a
Gillesedgy <edgygilles@pursuit.org>
2022-10-05 12:29:45 -0400

    Creating-Css

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -2,12 +2,12 @@ import ContactUserCard from "./ContactUserCard";
 
 const Contacts = () => {
   return (
-    <>
+    <div className="ConstactUserCard">
+      <h1>Contacts</h1>
       <ContactUserCard />
       <ContactUserCard />
       <ContactUserCard />
-      <ContactUserCard />
-    </>
+    </div>
   );
 };
 
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,10 +1,13 @@
+import "./NavBar.css";
 const NavBar = () => {
   return (
-    <header className="Header">
-      <button type="submit">What is Pursuit?</button>
-      <button type="submit">Creat an account</button>
-      <button type="submit">Sign in</button>
-    </header>
+    <div className="Header">
+      <header>
+        <button type="submit">What is Pursuit?</button>
+        <button type="submit">Creat an account</button>
+        <button type="submit">Sign in</button>
+      </header>
+    </div>
   );
 };
 
