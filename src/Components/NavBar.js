a223024af73fe3ca6d5169a6b3c1d5c3169591e4
i-guerrero <isaacguerrero@pursuit.org>
2022-10-04 22:08:10 -0400

    Passing all tests

---
+++ src/Components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <button>What is Pursuit?</button>
+                <button>Create an account</button>
+                <button>Sign in</button>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
---
+++ src/Components/Posts.js
@@ -0,0 +1,14 @@
+import Post from "./Post";
+
+const Posts = () => {
+  return (
+    <div>
+      Posts
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+};
+
+export default Posts;
