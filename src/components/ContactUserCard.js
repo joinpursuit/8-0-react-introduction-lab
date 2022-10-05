6022709e3e58471578fb685ad3319dfffed20318
Karyn Huston <huston.karyn@gmail.com>
2022-10-05 15:23:58 -0400

    git changes

---
+++ src/components/ContactUserCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const ContactUserCard = () => {
+    return (
+        <>
+        <div></div>
+        
+        </>
+    )
+}
+
+export default ContactUserCard
---
+++ src/components/Post.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Posts from "./Posts.js";
+const Post = () => {
+    return (
+        <>
+        <Posts/>
+        </>
+    )
+}
+
+export default Post
