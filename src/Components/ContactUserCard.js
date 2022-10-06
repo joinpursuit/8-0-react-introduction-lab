bb5a43496d35ff10ecf69a86cafd9088b8a4f737
Salinamalek <salinamalek@pursuit.org>
2022-10-06 15:39:31 -0400

    tests passed

---
+++ src/Components/ContactUserCard.js
@@ -0,0 +1,12 @@
+function ContactUserCard() {
+  return (
+    <div>
+      <img src="./profile.png"></img>
+      <h3>Name</h3>
+      <h5>Title</h5>
+      <p>Lorem Ipsum</p>
+    </div>
+  );
+}
+
+export default ContactUserCard;
