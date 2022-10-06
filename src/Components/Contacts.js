8c227dfb519a67f5d8636223b88a1acaa60eafd4
Richie-Pursuit <richiecarrasco@pursuit.org>
2022-10-06 01:56:58 -0400

    done but cypress is acting up

---
+++ src/Components/Contacts.js
@@ -0,0 +1,14 @@
+import ContactUserCard from `./ContactUserCard`;
+
+const Contacts = () => {
+    return(
+        <aside>
+            <ContactUserCard />
+            <ContactUserCard />
+            <ContactUserCard />
+            <ContactUserCard />
+        </aside>
+    )
+}
+
+export default Contacts;
---
+++ src/Components/Post.js
@@ -0,0 +1,10 @@
+const Post=()=>{
+    return(
+    <div>
+      <h4> Title</h4>
+      <p>lorem Ipsum</p>
+    </div>
+    )
+}
+
+export default Post;
