7cc5553ea0520bf5b4d88e31d1a8b6e396917fae
Lennie Nurse <lnursej@gmail.com>
2022-10-07 05:29:16 -0400

    "update"

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ const App = () => {
       <ul>
         <NavBar />
         <UserProfile />
+        <Posts />
+        <Contacts />
       </ul>
-      <Posts />
-      <Contacts />
     </div>
   )
 }
