b7ccea29c86534c2f98fa04e587bfc4afda71310
NRiddick113 <NatashaRiddick@pursuit.org>
2022-10-06 21:36:09 -0400

    css is off but it passed

--- src/Components/ContactUserCard.js
+++ src/Components/ContactUserCard.js
@@ -3,7 +3,7 @@ import "./ContactUserCard.css";
 function ContactUserCard() {
     return (
       <div id="ContactUserCard">
-        <img src="./profile.png" alt="ProfilePic"></img>
+        <img id="pic" src="./profile.png" alt="ProfilePic"></img>
         <h2>Jordan Walke</h2>
         <h3>React Creator</h3>
         <h4>Lorem Ipsem</h4>
