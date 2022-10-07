2c45468d23ec496869a470d75b71aa6f86d0f7cc
Lennie Nurse <lnursej@gmail.com>
2022-10-07 05:26:11 -0400

    "update"

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,5 +1,11 @@
 const Post = () => {
-  return <div className="Post">Post</div>
+  return (
+    <div className="Post">
+      Post
+      <h4 className="title">Title</h4>
+      <p className="lorem">Lorem Ipsum</p>
+    </div>
+  )
 }
 
 export default Post
