d16c0fe8f8938b1ac90436616554c91bb5b95363
cmm-7 <carlosmendoza@pursuit.org>
2022-10-08 23:45:16 -0400

    react-introduction-lab

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./index.css";
+import NavBar from "./components/NavBar";
+import UserProfile from "./components/UserProfile";
+import Posts from "./components/Posts";
+import Contacts from "./components/Contacts";
 
-function App () {
-  return <p>Hello, world!</p>;
-};
+function App() {
+  return (
+    <main>
+      <NavBar />
+      <UserProfile />
+      <Posts />
+      <Contacts />
+      <p>Hello, world!</p>
+    </main>
+  );
+}
 
 export default App;
---
+++ src/Components/Contacts.js
@@ -0,0 +1,13 @@
+import ContactUserCard from "./ContactUserCard";
+
+function Contacts() {
+  return (
+    <p>
+      Contacts
+      <br></br>
+      <ContactUserCard />
+    </p>
+  );
+}
+
+export default Contacts;
