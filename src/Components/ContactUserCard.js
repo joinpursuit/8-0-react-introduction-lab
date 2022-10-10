bdd28c8a1dbc7ad0d295fb7197f928a5fe22d7b6
Yiannar <yiannarichey@pursuit.org>
2022-10-10 17:41:19 -0400

    Passing Lab

---
+++ src/Components/ContactUserCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ContactUserCard() {
+  return (
+    <div>
+        <p>ContactUserCard</p>
+        </div>
+  )
+}
---
+++ src/Components/Contacts.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ContactUserCard from './ContactUserCard'
+
+export default function Contacts() {
+  return (
+    <div>
+        <p>Contacts</p>
+        <ContactUserCard />
+        <ContactUserCard />
+        <ContactUserCard />
+        <ContactUserCard />
+        </div>
+  )
+}
