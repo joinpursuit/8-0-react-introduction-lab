2fcdeeb31e1ecf6f7140105ef8d3c4e998d99fa4
pursuit-monique <moniquecorrea@pursuit.orgname>
2022-10-15 17:41:33 -0400

    Css incomplete

---
+++ src/Components/Contacts.js
@@ -0,0 +1,13 @@
+import ContactUserCard from "../Components/ContactUserCard";
+
+function Contacts() {
+  return (
+    <aside>
+      <ContactUserCard />
+      <ContactUserCard />
+      <ContactUserCard />
+    </aside>
+  );
+}
+
+export default Contacts;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,13 @@
+function NavBar() {
+  return (
+    <header>
+      <form>
+        <button>What is Pursuit?</button>
+        <button>Create an account</button>
+        <button>Sign in</button>
+      </form>
+    </header>
+  );
+}
+
+export default NavBar;
